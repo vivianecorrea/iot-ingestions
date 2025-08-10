0d12409451cb3f9e727e08e4f0e0bab66838b599
VIVIANE <viviane@MacBook-Air-de-VIVIANE.local>
2025-08-10 11:17:45 -0300

    feat: update producer

--- assignment-2/producer.py
+++ assignment-2/producer.py
@@ -4,20 +4,32 @@ from datetime import datetime, timezone
 from faker import Faker
 from confluent_kafka import Producer
 from time import sleep
+
+logging.basicConfig(
+    level=logging.INFO,  
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 class IotFakeProducer:
 
-    def __init__(self, configs: dict):
+    def __init__(self, configs: dict, seconds_run):
         self.configs = configs
+        self.seconds_run= seconds_run 
 
-
-    def _get_kafka_configs(configs: dict) -> dict:
-        return {
-        "bootstrap.servers": configs.get('BOOTSTRAP'),
-        "security.protocol": "SASL_SSL",
-        "sasl.mechanisms": "PLAIN",
-        "sasl.username": configs.get('API_KEY'),
-        "sasl.password": configs.get('API_SECRET'),
-    }
+    def run(self):
+        logging.info(f"Starting IotFakeProducer for {self.seconds_run} seconds")
+        end_time = time.time() + self.seconds_run
+        while time.time() < end_time:
+            self._produce_message()
+            sleep(1)
+    
+    def _produce_message(self):
+        producer = Producer(self._get_kafka_configs(self.configs))     
+        message = self._generate_fake_messages(random.randint(1, 1000))
+        producer.produce(self.configs.get('TOPIC'), value=message)
+        logging.info(f"Produced message: {message}, to topic: {self.configs.get('TOPIC')}")
+        producer.flush()
+        time.sleep(5)       
 
     def _generate_fake_messages(self, id: str):
         fake = Faker()
@@ -30,28 +42,24 @@ class IotFakeProducer:
         "lat": float(fake.latitude()),
         "lon": float(fake.longitude())
         }
-        #message = json.dumps(message).encode('utf-8')
+        message = json.dumps(message).encode('utf-8')
         logging.info(f"Produced message: {message}, to topic: {self.configs.get('TOPIC')}")
         return message
 
+    @staticmethod
+    def _get_kafka_configs(configs: dict) -> dict:
+        return {
+        "bootstrap.servers": configs.get('BOOTSTRAP'),
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanisms": "PLAIN",
+        "sasl.username": configs.get('API_KEY'),
+        "sasl.password": configs.get('API_SECRET'),
+    }
+
+   
+
          
 
-    def run(self):
-        producer = Producer(self._get_kafka_configs(self.configs))     
-        message = self._generate_fake_messages(random.randint(1, 1000))
-        producer.produce(self.configs.get('TOPIC'), value=message)
-        logging.info(f"Produced message: {message}, to topic: {self.configs.get('TOPIC')}")
-        producer.flush()
-        time.sleep(5)   
-
-
-instance = IotFakeProducer({
-    "BOOTSTRAP": "your_bootstrap_servers",
-    "API_KEY": "your_api_key",
-    "API_SECRET": "your_api_secret",
-    "TOPIC": "your_topic_name"
-})
-while True:
-    id = random.randint(1, 1000)
-    print(instance._generate_fake_messages(id))
-    sleep(5)
+    
+            
+    
